eeaa94bb1c803aafd717a80f71c7c5a32b27d2ba
jav1er83 <me@fjavieralba.com>
2022-01-25 15:31:17 +0100

    refactor to use new files/classes

--- src/main/java/ai/humn/telematics/ProcessDataFile.scala
+++ src/main/java/ai/humn/telematics/ProcessDataFile.scala
@@ -4,19 +4,7 @@ import scala.io.Source
 import scala.collection.Iterator
 
 
-case class Journey(journeyId: String,
-                   driverId: String,
-                   startTime: Long,
-                   endTime: Long,
-                   startLat: Float,
-                   startLon: Float,
-                   endLat: Float,
-                   endLon: Float,
-                   startOdometer: Long,
-                   endOdometer: Long)
-{
-  def duration: Long = endTime - startTime
-}
+
 
 object ProcessDataFile {
 
@@ -25,7 +13,7 @@ object ProcessDataFile {
     // Parse input CSV file into a list of Journeys
     val inputPath = args(0)
     val source = Source.fromFile(inputPath)
-    val journeys = parseJourneys(source)
+    val journeys = JourneyFileParser.parseJourneys(source)
 
     // 1. Find journeys that are 90 minutes or more.
     println("Journeys longer than 90 minutes:")
@@ -50,41 +38,4 @@ object ProcessDataFile {
     // 4. Find the most active driver - the driver who has driven the most kilometers.
 
   }
-
-
-  def parseJourneys(source: Source): Iterator[Journey] = {
-    val rawLines = source.getLines()
-    rawLines.next() // skip header
-    // Parse CSV Lines
-    val parsedLines = for (rawLine <- rawLines) yield parseCsvLine(rawLine)
-    val validLines = parsedLines.filter(isValid)
-    buildJourneys(validLines)
-  }
-
-  def parseCsvLine(line: String): Array[String] = {
-    line.split(",")
-  }
-
-  def isValid(parsedLine: Array[String]): Boolean = {
-    true
-  }
-
-  def buildJourneys(parsedLines: Iterator[Array[String]]): Iterator[Journey] = {
-    for (line <- parsedLines) yield buildJourney(line)
-  }
-
-  def buildJourney(csvFields: Array[String]): Journey = {
-    Journey(journeyId = csvFields(0),
-      driverId = csvFields(1),
-      startTime = csvFields(2).toLong,
-      endTime = csvFields(3).toLong,
-      startLat = csvFields(4).toFloat,
-      endLat = csvFields(5).toFloat,
-      startLon = csvFields(6).toFloat,
-      endLon = csvFields(7).toFloat,
-      startOdometer = csvFields(8).toLong,
-      endOdometer = csvFields(9).toLong
-    )
-  }
-
 }
