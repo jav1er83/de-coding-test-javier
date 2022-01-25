457891c18fb2adac0f8ccaf021ca15343719a499
jav1er83 <me@fjavieralba.com>
2022-01-25 16:56:58 +0100

    add helper mehtods to Journey, update main to print average speeds

--- src/main/java/ai/humn/telematics/Journey.scala
+++ src/main/java/ai/humn/telematics/Journey.scala
@@ -11,5 +11,19 @@ case class Journey(journeyId: String,
                    startOdometer: Long,
                    endOdometer: Long)
 {
+  /** Duration in ms of a journey */
   def duration: Long = endTime - startTime
+
+  /** Distance in Kms of a journey */
+  def distance: Double = endOdometer - startOdometer
+
+  /** Average speed of a journey in Km/h */
+  def avgSpeed: Double = {
+    val durationInHours = duration / (1000 * 60 * 60)
+    distance / durationInHours
+  }
+
+  override def toString: String = {
+    "journeyId: "+journeyId+" "+driverId+" distance "+distance+" durationMS "+duration+" avgSpeed in kph was "+avgSpeed
+  }
 }
--- src/main/java/ai/humn/telematics/ProcessDataFile.scala
+++ src/main/java/ai/humn/telematics/ProcessDataFile.scala
@@ -9,19 +9,21 @@ object ProcessDataFile {
     // Parse input CSV file into a list of Journeys
     val inputPath = args(0)
     val source = Source.fromFile(inputPath)
-    val journeys = JourneyFileParser.parseJourneys(source)
+    val journeys = JourneyFileParser.parseJourneys(source).toList
+    source.close()
 
     // 1. Find journeys that are 90 minutes or more.
     println("Journeys longer than 90 minutes:")
     for (journey <- journeys) {
       val ninetyMins = 90 * 60 * 1000
-      if (journey.duration > ninetyMins) println(journey.journeyId)
+      if (journey.duration > ninetyMins) println(journey)
     }
 
-    source.close()
-
-    // need to do the
     // 2. Find the average speed per journey in kph.
+    println("Average speed per (valid) journey:")
+    for (journey <- journeys) println(journey)
+
+
 
     // her eis where I will
     // 3. Find the total mileage by driver for the whole day.
