d05a47cdf3ce5591a007b895c2ca468bba91832f
jav1er83 <me@fjavieralba.com>
2022-01-31 15:50:25 +0100

    remove comments

--- src/main/java/ai/humn/telematics/model/Journey.scala
+++ src/main/java/ai/humn/telematics/model/Journey.scala
@@ -10,34 +10,29 @@ case class Journey(journeyId: String,
                    endLon: Double,
                    startOdometer: Double,
                    endOdometer: Double) {
-  /** Duration in ms of a journey */
-  def duration: Double = endTime - startTime
 
-  /** Distance in Kms of a journey */
-  def distance: Double = endOdometer - startOdometer
+  def durationMs: Double = endTime - startTime
 
-  /** Average speed of a journey in Km/h */
-  def avgSpeed: Double = {
+  def distanceKm: Double = endOdometer - startOdometer
+
+  def avgSpeedKph: Double = {
     // If no distance was travelled, speed will be 0:
-    if (distance <= 0) return 0.0
+    if (distanceKm <= 0) return 0.0
 
     // If no duration is recorded for the trip, we cannot compute speed (it would be infinite).
     // We also return 0 in this case.
-    if (duration <= 0) return 0.0
+    if (durationMs <= 0) return 0.0
 
     // For usual cases where we have valid distance and duration, we just
     // transform trip duration to hours and divide distance/duration since we want to return Km/h:
-    val durationInHours = duration / (1000 * 60 * 60)
-    distance / durationInHours
+    val durationInHours = durationMs / (1000 * 60 * 60)
+    distanceKm / durationInHours
   }
 
-  /** Validates a Journey */
-  def isValid: Boolean = {
-    duration >= 0 & distance >= 0
-  }
+  def isValid: Boolean = durationMs >= 0 & distanceKm >= 0
 
   def summary: String = {
-    val durationInt = duration.toInt
-    "journeyId: "+journeyId+" "+driverId+" distance "+f"$distance%5s"+" durationMS "+f"$durationInt%8s"+" avgSpeed in kph was "+f"$avgSpeed%1.2f"
+    val durationInt = durationMs.toInt
+    "journeyId: "+journeyId+" "+driverId+" distance "+f"$distanceKm%5s"+" durationMS "+f"$durationInt%8s"+" avgSpeed in kph was "+f"$avgSpeedKph%1.2f"
   }
 }
